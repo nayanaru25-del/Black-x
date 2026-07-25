ee621347acc2c1b558a0535f1cc00b4ee12830d7
Nayan Aru <nayanaru@Mac.lan>
2026-07-25 13:01:21 +0530

    Updated website

--- src/pages/Mastercard.jsx
+++ src/pages/Mastercard.jsx
@@ -2,68 +2,60 @@ import PackageCard from "../components/PackageCard";
 
 function Mastercard() {
   return (
-    <section className="package-page">
-      <div className="package-header">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg"
-          alt="Mastercard"
-          className="package-logo"
-        />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexWrap: "wrap",
+        gap: "25px",
+        padding: "120px 20px 50px",
+        background: "#090909",
+        minHeight: "100vh",
+      }}
+    >
+      <PackageCard
+        logo="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg"
+        name="Mastercard Basic"
+        price="650"
+      />
 
-        <h1>Mastercard Packages</h1>
+      <PackageCard
+        logo="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg"
+        name="Mastercard Silver"
+        price="850"
+      />
 
-        <p>
-          Choose the package that best suits your needs.
-        </p>
-      </div>
+      <PackageCard
+        logo="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg"
+        name="Mastercard Gold"
+        price="999"
+        popular={true}
+      />
 
-      <div className="package-grid">
+      <PackageCard
+        logo="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg"
+        name="Mastercard Platinum"
+        price="1299"
+      />
 
-        <PackageCard
-          logo="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg"
-          name="Mastercard Basic"
-          price="650"
-        />
+      <PackageCard
+        logo="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg"
+        name="Mastercard Diamond"
+        price="1699"
+      />
 
-        <PackageCard
-          logo="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg"
-          name="Mastercard Silver"
-          price="850"
-        />
+      <PackageCard
+        logo="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg"
+        name="Mastercard Elite"
+        price="2199"
+      />
 
-        <PackageCard
-          logo="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg"
-          name="Mastercard Gold"
-          price="999"
-          popular={true}
-        />
-
-        <PackageCard
-          logo="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg"
-          name="Mastercard Platinum"
-          price="1299"
-        />
-
-        <PackageCard
-          logo="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg"
-          name="Mastercard Diamond"
-          price="1699"
-        />
-
-        <PackageCard
-          logo="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg"
-          name="Mastercard Elite"
-          price="2199"
-        />
-
-        <PackageCard
-          logo="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg"
-          name="Mastercard Black"
-          price="2999"
-        />
-
-      </div>
-    </section>
+      <PackageCard
+        logo="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg"
+        name="Mastercard Black"
+        price="2999"
+      />
+    </div>
   );
 }
 
